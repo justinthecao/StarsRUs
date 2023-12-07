b50d6a2762803716aca6d824a1d99fffb19df837
Justin Cao <justincao@Justins-MacBook-Pro.local>
2023-12-06 22:36:43 -0800

    save

--- extra.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO CurrentDate VALUES (DATE '2023-10-30');
-
-INSERT INTO MarketAccountHistory VALUES (DATE '2023-10-1', 100, 2);
-INSERT INTO MarketAccountHistory VALUES (DATE '2023-10-10', 200, 2);
-INSERT INTO MarketAccountHistory VALUES (DATE '2023-10-20', 300, 2);
-
-SELECT *
-FROM MarketAccountHistory
